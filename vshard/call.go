bed8f8cf7d18abddb35f4f4c7d35a60ae23f56d5
maxim-konovalov <maksim.konovalov@vk.team>
2024-01-01 21:54:24 +0300

    vshard-1: next call step

--- vshard/call.go
+++ vshard/call.go
@@ -6,20 +6,27 @@ import (
 	"github.com/tarantool/go-tarantool/v2"
 )
 
-func (r *Router) Call(shardKey fmt.Stringer, fnc string, args interface{}) error {
+func (r *Router) Call(shardKey fmt.Stringer, fnc string, args interface{}) (bool, error) {
 	bucketID := r.RouterBucketIDStrCRC32(shardKey.String())
 
 	return r.CallWithBucketID(bucketID, fnc, args)
 }
 
-func (r *Router) CallWithBucketID(bucketID uint64, fnc string, args interface{}) error {
+func (r *Router) CallWithBucketID(bucketID uint64, fnc string, args interface{}) (bool, error) {
 	inst := r.BucketResolve(bucketID)
+	callStatus := new(bool)
 
 	req := tarantool.NewCallRequest("vshard.storage.call")
 	req = req.Args([]interface{}{bucketID, "write", fnc, args}) // add args
 
-	future := inst.conn.Do(req)
-	fmt.Println(future.Get())
+	var result interface{}
 
-	return nil
+	future := inst.conn.Do(req)
+	// Vshard.storage.call(func) returns two values: true/false and func result.
+	err := future.GetTyped(&[]interface{}{callStatus, &result})
+	if err != nil {
+		return false, err
+	}
+
+	return false, err
 }
